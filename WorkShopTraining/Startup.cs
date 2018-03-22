b8cabb0209139cbb1101e4ea75683b3c29804673
庭瑋 黃 <tttxxx52@gmail.com>
2018-03-22 15:29:12 +0800

    加入專案檔案。

---
+++ WorkShopTraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkShopTraining.Startup))]
+namespace WorkShopTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
