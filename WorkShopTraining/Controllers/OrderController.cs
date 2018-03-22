a72666bb2527149934ea0ad823a44e1605c3af3d
庭瑋 黃 <tttxxx52@gmail.com>
2018-03-22 16:22:51 +0800

    123321

---
+++ WorkShopTraining/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkShopTraining.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            List<Models.Order> dataList = orderService.GetEmployeeData();
+            List<SelectListItem> employeeList = new List<SelectListItem>();
+            List<SelectListItem> shipperList = new List<SelectListItem>();
+
+            //員工List
+            foreach (var item in dataList)
+            {
+                employeeList.Add(new SelectListItem()
+                {
+                    Text = item.EmployeeName,
+                    Value = item.EmployeeID.ToString(),
+                });
+            }
+            ViewBag.empData = employeeList;
+
+            //供應商List
+            dataList = orderService.GetShipperData();
+            foreach (var item in dataList)
+            {
+                shipperList.Add(new SelectListItem()
+                {
+                    Text = item.ShipperName,
+                    Value = item.ShipperID.ToString()
+                });
+            }
+
+            ViewBag.shipperData = shipperList;
+            return View();
+        }
+
+        [HttpPost()]
+        public ActionResult GetData(Models.Order order)
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WorkShopTraining/Models/OrderService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WorkShopTraining.Models
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 連結資料庫
+        /// </summary>
+        /// <returns></returns>
+        private string GetDBConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
+        }
+
+
+        /// <summary>
+        /// 取得員工資料
+        /// </summary>
+        public List<Models.Order> GetEmployeeData()
+        {
+            DataTable dataTable = new DataTable();
+            string sql = @"SELECT EmployeeID, FirstName + ' ' + LastName As EmployeeName
+                         FROM HR.Employees";
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dataTable);
+                conn.Close();
+            }
+            return this.MapEmployeeDataToList(dataTable);
+        }
+
+        /// <summary>
+        /// 取得員工資料變成List
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        private List<Order> MapEmployeeDataToList(DataTable dataTable)
+        {
+            List<Models.Order> result = new List<Order>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                result.Add(new Order
+                {
+                    EmployeeID = (int)row["EmployeeID"],
+                    EmployeeName = row["EmployeeName"].ToString()
+                });
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 取得供應商資料
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetShipperData()
+        {
+            DataTable dataTable = new DataTable();
+            string sql = @"SELECT ShipperID, CompanyName As ShipperName
+                           FROM Sales.Shippers";
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dataTable);
+                conn.Close();
+            }
+            return this.MapShipperDataToList(dataTable);
+        }
+
+        /// <summary>
+        /// 取得供應商資料變成List
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        private List<Order> MapShipperDataToList(DataTable dataTable)
+        {
+            List<Models.Order> result = new List<Order>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                result.Add(new Order
+                {
+                    ShipperID = (int)row["ShipperID"],
+                    ShipperName = row["ShipperName"].ToString()
+                });
+            }
+            return result;
+        }
+
+
+    }
+}
